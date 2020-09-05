907cbcb844c243dfdc42cdbd5314753e4ebbd9cb
Fernando Pineda <fpineda@intercorp.com.pe>
2020-09-05 14:19:55 -0500

    Initial commit

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+function Footer( ) {
+    return (
+        <div className="footer">
+            <div className="pure-menu pure-menu-horizontal">
+                <ul>
+                    <li className="pure-menu-item"><a href="https://www.linkedin.com/in/fpineda3105/" className="pure-menu-link">About</a></li>
+                    <li className="pure-menu-item"><a href="https://twitter.com/_fpineda/" className="pure-menu-link">Twitter</a></li>
+                    <li className="pure-menu-item"><a href="https://github.com/fpineda3105" className="pure-menu-link">GitHub</a></li>
+                </ul>
+            </div>
+        </div> 
+    );    
+}
+
+export default Footer
